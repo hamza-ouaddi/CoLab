5b04c3fd39d3350b0d796440d096210636a7c911
hamza-ouaddi <hamza0addi@gmail.com>
2023-09-04 16:10:51 +0100

    show manage and logout links after login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,14 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'User created and logged in successfuly.');
     }
+
+    //Log user out
+    public function logout(Request $request){
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out.');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,6 @@ Route::get('/register', [UserController::class, 'create']);
 
 //Create New User
 Route::post('/users', [UserController::class, 'store']);
+
+//Log User Out
+Route::post('/logout', [UserController::class, 'logout']);
