2a334350b872bca9d6b4333da7814e323c6bf6ab
hamza-ouaddi <hamza0addi@gmail.com>
2023-09-06 11:35:32 +0100

    fix responsive

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,21 +1,21 @@
 @props(['listing'])
 
 <div class="md:h-[240px] h-[150px] bg-gradient-to-br from-lightGray to-gray rounded-3xl md:p-6 p-3">
-    <div class="flex gap-6">
-        <div class="flex-none bg-white rounded-xl p-10 md:w-48 w-32 md:h-48 h-32 flex items-center flex-shrink-0">
+    <div class="flex  md:gap-6 gap-4">
+        <div class="flex-none bg-white rounded-xl md:p-10 p-5 md:w-48 w-32 md:h-48 h-32 flex items-center flex-shrink-0">
             <img src="{{ $listing->logo? asset('storage/' . $listing->logo) : asset('/images/no-image.png') }}" alt="company-logo" class="">
         </div>
-        <div class="flex-grow">
-            <h3 class="md:text-[2rem] text-[1.4rem] text-background font-semibold md:leading-8 leading-6 md:mb-[7px] mb-0.5">
+        <div class="">
+            <h3 class=" md:text-[2rem] text-base text-background font-semibold md:leading-8 leading-6 md:mb-[7px] mb-0.5">
                 <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="md:text-2xl text-sm text-background font-light md:leading-[34px] leading-6 md:mb-[14px] mb-2">{{$listing->company}} </div>
             
-            <ul  class="flex mb-[7px] md:gap-4 gap-[10px]">
-                    <x-listing-tags :tags-csv="$listing->tags" class="flex items justify-center bg-white text-black rounded-2xl py-0.5 px-4 font-normal md:text-sm text-[0.5rem]"/>
+            <ul  class="flex flex-wrap md:gap-4 gap-2">
+                    <x-listing-tags :tags-csv="$listing->tags" class="flex items-center justify-center bg-white text-black rounded-2xl py-0.5 md:px-4 px-2 font-normal md:text-sm text-xs"/>
             </ul>
             
-            <div class=" text-background md:text-base text-[10px] mt-2 font-light leading-8">
+            <div class=" text-background md:text-base text-[10px] mt-2 font-light ">
                 <i class="fa-solid fa-location-dot mr-2"></i>{{$listing->location}}
             </div>
         </div>
